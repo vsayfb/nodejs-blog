0c3932bcad6a4e94b536bde93fbcec024811e0b2
vsay <vsayfb@gmail.com>
2022-02-08 16:49:41 +0300

    design comment-area

--- public/js/commentArea.js
+++ public/js/commentArea.js
@@ -1,12 +1,72 @@
 const e = React.createElement;
 
-class CommentArea extends React.Component {
+class AddComment extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  addComment = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  render() {
+    return e(
+      "div",
+      { className: "mt-4" },
+      e(
+        "form",
+        {
+          onSubmit: this.addComment,
+        },
+        e(
+          "label",
+          { className: "form-label", htmlFor: "text" },
+          "Your comment."
+        ),
+
+        e("textarea", {
+          className: "form-control",
+          name: "text",
+        }),
+        e(
+          "button",
+          { type: "submit", className: "btn btn-warning mt-3" },
+          "Comment"
+        )
+      )
+    );
+  }
+}
+
+class Comments extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return e("div", {}, "Hello World");
+    return e(
+      "div",
+      {
+        className: "comment p-3 border",
+      },
+      e("div", null, " vsay "),
+      e("p", { className: "m-0 mt-1" }, "Example comment. ")
+    );
+  }
+}
+
+class CommentArea extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  addComment = () => {
+    alert("hi");
+  };
+
+  render() {
+    return e("div", {}, e(Comments), e(AddComment));
   }
 }
 
